20418609b004f77a5b68f30115ebe7559772ae0e
utkarsho08 <utkarsh08nov@gmail.com>
2025-08-01 14:18:29 +0530

    port added

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,19 @@ const postRoutes = require('./routes/posts');
 const topicRoutes = require('./routes/topics');
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connection established successfully ✅'))
-    .catch(err => console.error('🔴 MongoDB connection error:', err.message));
-
+    .then(() => console.log('MongoDB connection established successfully'))
+    .catch(err => console.error('MongoDB connection error:', err.message));
 
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/topics', topicRoutes);
 
-
-module.exports = app;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
